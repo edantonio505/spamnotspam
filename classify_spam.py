8bf7ce2152ec59d66606895c0a20f35c0d03f15b
Edgar Velazquez <edantonio505@gmail.com>
2018-12-09 03:45:49 -0500

    th

--- classify_spam.py
+++ classify_spam.py
@@ -1,22 +1,25 @@
 import argparse
 import pickle
+from spamClassifier import spamClassifier
 
+spamclf = spamClassifier()
 
 
 def main():
     parser = argparse.ArgumentParser(description="Classify email in spam or not spam")
     parser.add_argument("--message","-m",  help="Email message")
     args = parser.parse_args()
-        
     message = args.message
-    
     if not message:
         print("Please enter a email message")
         parser.parse_args(["-h"])
-    
-
-
-
+    else:
+        labels = [
+            "ham",
+            "spam"
+        ]
+        result = spamclf.classify(message)[0]
+        print(labels[result])
 
 
 if __name__ == "__main__":
